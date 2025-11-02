3ffb8184caf5395a6bf16f5cd0877c09902ace2a
chetan arora <56200601+masterchetan11@users.noreply.github.com>
2025-11-02 18:00:19 +0000

    Add FastAPI product endpoints and product model

---
+++ models.py
@@ -0,0 +1,14 @@
+class product:
+    id:int
+    name:str
+    description:str
+    price:float
+    quantity:int
+
+def __init__(self, id,name,description,price,quantity):
+    self.id=id
+    self.name=name
+    self.description=description
+    self.price=price
+    self.quantity=quantity  
+        
