84aacce34afd205c200f14ce1ab54236b1461609
chetan arora <56200601+masterchetan11@users.noreply.github.com>
2025-11-03 10:05:29 +0000

    update

--- main.py
+++ main.py
@@ -9,10 +9,28 @@ def greet():
     return " this is fast api"
 
 products = [
-    product(name="laptop",description="dell ",price=45000,quantity=5),
-    product(name="phone",description="apple",price=70000,quantity=10), 
+    product(id = 1,name = "laptop",description="dell ",price=45000,quantity=5),
+    product(id = 2,name = "phone",description="apple",price=70000,quantity=10), 
 ]
 
 @app.get("/products")
 def _products():
     return products
+@app.get("/products/{id}")
+def get_products_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product
+    return "product not found"
+@app.post("/products")
+def add_product(product:product):
+    products.append(product)
+    return products 
+
+@app.update("/product")
+def update_product(id:int ,product:product):
+    for i in range (len(products)):
+        if products[0].id ==product:
+            return "product added  successfully"
+        
+    return "product not found"
